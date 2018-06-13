f0c130757935303c4185f26bea5ef3b63ab0462e
ThaimMwaeLwin <thaimhmwae19>
2018-06-13 22:10:03 +0630

    pythonex38

--- ex19.py
+++ ex19.py
@@ -11,7 +11,7 @@ print("OR, we can use variables from our script:")
 amount_of_cheese = 10
 amount_of_crackers = 50
 
-cheese_and-crackers(amount_of_cheese, amount_of_crackers)
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
 print("We can even do math inside too:")
 cheese_and_crackers(10 + 20, 5 + 6)

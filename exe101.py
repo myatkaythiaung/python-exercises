8efd9c0ccc7dd8b797384e3eac1589a93bcaadf7
myatkaythiaung <39325198+myatkaythiaung@users.noreply.github.com>
2018-06-26 15:00:36 +0630

    Update exe101.py

--- exe101.py
+++ exe101.py
@@ -1,13 +1,13 @@
-tabby_rat = "\tI'm tabbed in."
-persian_rat = "I'm split\non a line."
-backslash_rat = "I'm \\ a \\ dog."
+tabby_as = "\tI'm tabbed in."
+persian_as = "I'm split\non a line."
+backslash_as = "I'm \\ a \\ dog."
 fat_dog = """
 I'll do a list:
     \t* Dog food
     \t* Fishies
     \t* Catnip\n\t* Grass
     """
-print(tabby_rat)
-print(persian_rat)
-print(backslash_rat)
-print(fat_rat)
+print(tabby_as)
+print(persian_as)
+print(backslash_as)
+print(fat_as)
